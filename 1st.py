2e89b8a0a9216163e91d0622064aaa32dfada6f1
Dileep Choudhary <dileepchoudhary1008@gmail.com>
2025-10-01 22:55:43 +0530

    Add files via upload

---
+++ 1st.py
@@ -0,0 +1,103 @@
+# name = "Dileep"
+# age = 19
+# price = 259.33
+
+# print("My name is :", name)
+# print("my age is :", age)
+# print(type(name))
+# print(type(age))
+# print(type(price))
+# age = 19 
+# old = False 
+# a = None 
+
+# print(type(age))
+
+# print(type(old))
+# print(type(a))
+# a = 25
+# b = 65
+
+# sum = a+b 
+# print(sum)
+# arithmetic operators
+# a = 5
+# b = 6
+# print (a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b) #remainder
+# print(a**b) #a^b
+# print(type(a+b))
+# print(type(a-b))
+# print(type(a*b))
+# print(type(a/b))
+# print(type(a%b))
+# print(type(a**b))
+
+# a = 64
+# b = 45
+
+
+# print( a==b)
+# print(a != b)
+# print(a>=b) #True
+# print(a<=b) # False
+# print(a>b)#True
+# print(a<b)#False
+
+# assignment operators
+
+# num = 8
+# num += 12
+
+# print("num :", num)#8
+# # 
+# logical operators
+# print( not False )
+# print( not True)
+
+
+# val1 = True
+# val2 =True
+
+# print("OR operators :" , val1 and val2)
+
+# Input in Python
+
+
+# name = input("enter your name :")
+# age = input("enter your age:")
+
+
+
+# print( "you are in right time and be positive :", age)
+# print("welcome:", name)
+
+# write a program to input 2 numbers and print their sum 
+
+# first = int(input("enter first:"))
+# second = int(input("enter second :"))
+
+# print("sum:",first + second)
+
+# write a program t input side of a square and print its area
+
+# side = float(input("enter square side :"))
+
+# print("area:" , side*side)
+
+# write a program to input 2 floating point numbers and print their avg
+
+# a = float(input("enter x :"))
+# b = float(input("enter y :"))
+
+
+
+# write a program to input 2 int numbers a and b
+
+# a = int(input("enter x :"))
+# b = int(input("enter y :"))
+
+# print(a >= b)
