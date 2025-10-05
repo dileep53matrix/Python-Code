8565b7ab38c01543823046c52ee409caa13ce8d7
Dileep Choudhary <dileepchoudhary1008@gmail.com>
2025-10-05 15:38:08 +0530

    Add files via upload

---
+++ 3rd.py
@@ -0,0 +1,76 @@
+#list and Tuple
+#list in python
+
+# marks = [12,54,5,4,5,22,55]
+# print(marks[5])
+# print(marks[2])
+
+# name = ["Tejprakash","Koushalya", "Menka", "Ankita", "Pooja", "nothing"]
+
+# name[5] = "Dileep"
+
+# print(len(name))
+# print(len(name[0]))
+
+# print(name[5])
+#list slicing 
+
+# marks = [11,12,22,55,88,11,45,45,35,35,36,24,45,2,55]
+
+# print(marks[0:])
+# print(marks[2:])
+# print(marks[0:10])
+# print(marks[:5])
+
+# print(marks[2:len(marks)])
+# print(marks[-11:-8])
+
+
+# list = [11,12,22,55,88,11,45,45,35,35,36,24,45,2,55]
+
+# print(len(list))
+
+# print(list.append(222))
+# print(list.sort())
+# print(list.sort(reverse = True))
+
+# print(list.reverse())
+
+# print(list.insert(11 , 444))
+
+# print(list.remove(55))
+# print(list.pop(15))
+# print(list)
+
+# print(len(list))
+
+
+#Tuples in python
+
+# tup = (11,11,22,5,48,4,44,5,8,4,459,8,44,4,55,55,4,5,44,55,5,8)
+# # tup[5] = 654
+# print(tup.index(5))
+
+# print(tup.count(5))
+# print(type(tup))
+
+# print(len(tup))
+
+#lets practice
+
+# list = []
+# list1 = input("enter first movie name:")
+# list2 = input("enter second movie name:")
+# list3 = input("enter third movie name:")
+
+# list.append(list1)
+# list.append(list2)
+# list.append(list3)
+
+# print(list)
+# list = []
+tup = ["c" ,"d", "a", "a", "b", "b" , "a"]
+list = tup
+print(list.sort())
+print(list)
+# print(tup.count("a"))  
