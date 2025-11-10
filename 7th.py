52a1f7ee32b795c42c5f3105bcf20401c581ba1b
Dileep Choudhary <dileepchoudhary1008@gmail.com>
2025-11-10 18:04:44 +0530

    Add files via upload

---
+++ 7th.py
@@ -0,0 +1,15 @@
+# #file I/O in python
+# f = open("free.txt", "r")
+# data = f.read()
+# print(data)
+# print(type(data))
+# f.close()
+# f = open("free.txt", "w")
+# f.write("\nFuture Hero")
+# f.close
+
+# import os
+# os.remove("free.txt")
+
+# f = open("demo.txt", "a")
+
