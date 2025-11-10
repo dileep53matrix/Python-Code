ad2530e0d968d7de3940547e4831b719b01f43be
Dileep Choudhary <dileepchoudhary1008@gmail.com>
2025-11-10 23:55:54 +0530

    Add files via upload

---
+++ 5th-Dileep.py
@@ -0,0 +1,36 @@
+# lecture  Loops
+# i = 1 
+# while i <= 100:
+#     print("Dileep", i)
+#     i += 1
+# print("end")
+
+# while True:
+#     print("Dileep")
+# print("end")
+# i = 1 
+# while i <= 5:
+#     print("hello")
+#     i += 1
+
+# i = 100 
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+#print the multification table of a number n
+
+# i = 1
+# while i <= 10:
+#     print(i*3)
+
+#     i += 1
+
+# print the elements of the following list using a loop
+
+nums = [1,4,9,16,25,36,49,64,81,100]
+print(len(nums))
+idx = 0
+while idx < len(nums):
+    print()
+    idx += 1
