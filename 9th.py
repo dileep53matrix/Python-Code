389e200d14e42e3413b0757753abdd9b3bbc2726
Dileep Choudhary <dileepchoudhary1008@gmail.com>
2025-12-14 23:42:14 +0530

    Add files via upload

---
+++ 9th.py
@@ -0,0 +1,18 @@
+
+# from helper_functions import print_llm_response
+# print_llm_response("What is the capital of France?")
+# name = "Otto Matic"
+# dog_age = 21/7
+# print_llm_response(f"""If {name} were a dog, he would be {dog_age} years old.
+# Describe what life stage that would be for a dog and what that might 
+# entail in terms of energy level, interests, and behavior.""")
+# driver = "unicorn"
+# drivers_vehicle = "colorful, asymmetric dinosaur car"
+# favorite_planet = "Pluto"
+# Fix this code
+# favorite_book1 = "1001 Ways to Wear a Hat"
+# second_fav_book = "2002 Ways to Wear a Scarf"
+# print(f"My most favorite book is {favorite_book1}, but I also like {second_fav_book}")
+age  = 19 
+name  = "Dileep"
+print(f"My name is {name} and i am {age} years old")
