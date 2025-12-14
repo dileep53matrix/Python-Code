11a42e5aca0dfe3059c7b540a9f8b1d654a468c4
Dileep Choudhary <dileepchoudhary1008@gmail.com>
2025-12-14 23:42:51 +0530

    Add files via upload

---
+++ 10th.py
@@ -0,0 +1,15 @@
+# print("hello world")
+# print(len("hello world"))
+# print(round(53.24))
+# string_length  = len("Dileep is a web develper")
+# print(string_length)
+name = "Tommy"
+potatoes = 4.75
+prompt = f"""Write a couplet about my friend {name} who has about {round(potatoes)} potatoes"""
+
+print(prompt)
+
+print(f"how are you doing my friend {name} , buy {round(potatoes)} kg of potatoes")
+
+lucky_number = 53
+print(f"my luckey number is {lucky_number}")
