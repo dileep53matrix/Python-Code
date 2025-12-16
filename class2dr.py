1b6d49725ce5cfea63e60e1e1b74ae9080e12176
Dileep Choudhary <dileepchoudhary1008@gmail.com>
2025-12-17 00:05:26 +0530

    Implement element removal from the list
    Added functionality to remove an element from the list if it exists.

--- class2dr.py
+++ class2dr.py
@@ -6,4 +6,13 @@ el = int(input("enter the element"))
 position = int(input("enter the position number:"))
 
 list.insert(position , el)
+
 print("after insert the el the list:", list)
+# for remove the el 
+if el in list:
+    list.remove(el)
+    print("list after removing el", list)
+else:
+    print("the el not i list")
+  
+  
